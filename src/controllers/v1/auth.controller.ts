40b6137f5a88151792725e47ad14b3968ae7f98b
Steven Mosquera <129177391+smosquerap@users.noreply.github.com>
2023-04-03 14:23:15 -0500

    feature: auth module

---
+++ src/controllers/v1/auth.controller.ts
@@ -0,0 +1,28 @@
+import { JsonController, Post, Body } from "routing-controllers";
+import { sign } from "jsonwebtoken";
+import { compare } from "bcryptjs";
+
+import { User } from "../../models/v1/user.model";
+
+@JsonController("/auth")
+export class AuthController {
+    @Post("/login")
+    async login(@Body() body: { email: string; password: string }) {
+        try {
+            const user = await User.findOneBy({ email: body.email });
+            if (!user) return { error: "El usuario no existe" };
+
+            const isPasswordCorrect = await compare(body.password, user.password);
+            if (!isPasswordCorrect) return { error: "Contraseña incorrecta" };
+
+            const token = sign({ ...body }, process.env.JWT_SECRET, { expiresIn: '10h' });
+            return {
+                ...body,
+                token
+            }
+        } catch (error) {
+            return { error }
+        }
+        
+    }
+}
