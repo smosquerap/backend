bb7a9c94719cbc2e8b020656ebed2a2381271599
Steven Mosquera <129177391+smosquerap@users.noreply.github.com>
2023-05-24 07:11:48 -0500

    refactor: errors management improved

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -4,10 +4,13 @@ import { ExpressErrorMiddlewareInterface, Middleware } from "routing-controllers
 export class errorHandler implements ExpressErrorMiddlewareInterface {
     async error(error: any, _request: any, response: any, next: any){
         if (error.errors) {
-            const errors = error.errors.map((err: { property: string; }) => `Field ${err.property} is required`);
-            response.status(error.httpCode).send({ message: `Invalid body`, errorCode: error.httpCode, errors });
-        } else {            
-            response.status(error.httpCode).json(error.message);
+            const errors = <any>{};
+            error.errors.map((err: { property: string; }) => {
+                errors[err.property] = `Field ${err.property} is required`
+            });
+            response.status(error.httpCode).send(errors);
+        } else {    
+            response.status(error.httpCode).json({ error: error.message });
         }
         next(error);
     }
